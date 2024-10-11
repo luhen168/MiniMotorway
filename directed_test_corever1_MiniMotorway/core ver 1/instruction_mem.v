46ea4129e0d2ab2894b43c9c76abe8fc2bee2159
luhen168 <trung2992003@gmail>
2024-10-11 18:52:39 +0700

    [Modify] Only rtl

--- directed_test_corever1_MiniMotorway/core ver 1/instruction_mem.v
+++
@@ -1,8 +0,0 @@
-module instruction_mem (
-    input [31:0] i_addr,
-    output [31:0] o_instr
-);
-    reg [31:0] rom [0:16383];
-    assign o_instr = rom[i_addr[31:2]];
-                   
-endmodule
--- rtl_minimotorway/if_id_reg.v
+++ rtl_minimotorway/if_id_reg.v
@@ -20,25 +20,20 @@ module if_id_reg(
             o_id_p4    <= 1'b0;
             o_id_pc    <= 1'b0;
             o_id_instr <= 1'b0;
-        end
-
-        else if ((current_flush == 1'b1) || (next_flush == 1'b1))
-        begin
-            o_id_p4    <= i_if_p4;
-            o_id_pc    <= i_if_pc;
-            o_id_instr <= 32'h00000013; // add x0, x0, 0
-        end
-
-        else
-        begin
+        end else begin 
             if(i_we)
             begin
                 o_id_p4    <= i_if_p4;
                 o_id_pc    <= i_if_pc;
-                o_id_instr <= i_if_instr;
+                if ((current_flush == 1'b1) || (next_flush == 1'b1))
+                    o_id_instr <= 32'h00000013; // add x0, x0, 0
+                else
+                    o_id_instr <= i_if_instr;
+            end
+            else 
+            begin
+                // o_id_instr <= i_if_instr; 
             end
         end
     end
-
-
 endmodule
